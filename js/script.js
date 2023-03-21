ba416841273f93386fc8758535abbb46cfc6219a
maggi-bratton <maggi.meins@gmail.com>
2023-03-21 13:49:49 -0600

    Fetch and display repo list

--- js/script.js
+++ js/script.js
@@ -4,13 +4,16 @@
 const overview = document.querySelector(".overview");
 //username
 const username = "maggi-b";
+//ul to display the repos list
+const repoList = document.querySelector(".repo-list");
 
-//API function to get info from GitHub profile
+//API function to get user info from GitHub profile
 const gitUserInfo = async function () {
     const response = await fetch(`https://api.github.com/users/${username}`);
     const data = await response.json();
     displayUserInfo(data);
 };
+
 gitUserInfo();
 
 //Display user info fetched from API function above
@@ -28,4 +31,24 @@ const displayUserInfo = function (data) {
   </div> `
 
     overview.append(div);
+    gitRepoList();
 };
+
+//API function to get list of repos from GitHub
+const gitRepoList = async function () {
+    const fetchRepos = await fetch (`https://api.github.com/users/${username}/repos?sort=updated&per_page=100`);
+    const repoData = await fetchRepos.json();
+    displayRepoInfo(repoData);
+};
+
+//function to display repo info
+const displayRepoInfo = function (repos) {
+    for (const repo of repos) {
+        const repoItem = document.createElement("li");
+        repoItem.classList.add("repo");
+        repoItem.innerHTML = `<h3>${repo.name}</h3>`;
+        repoList.append(repoItem);
+    }
+};
+
+
